105d1a3cd18086e3c4aa96f61bdf9f3098b1d129
Jan S <schaffranek.jan@web.de>
2021-01-30 16:40:39 +0100

    up

--- 3_CMake/13_CommandLineArguments/app/main.cc
+++ 3_CMake/13_CommandLineArguments/app/main.cc
@@ -1,22 +1,28 @@
 #include <iostream>
 
-#include "my_lib.h"
 #include "loguru.hpp"
 
-#include "ProjectConfig.h"
+#include "my_lib.h"
 
 int main(int argc, char **argv)
 {
-    std::cout << PROJECT_VERSION_MAJOR << "."
-              << PROJECT_VERSION_MINOR << "."
-              << PROJECT_VERSION_PATCH << std::endl;
-
     cout_hello_world();
     print_linalg_vector();
     print_boost_version();
 
     loguru::init(argc, argv);
-    LOG_F(INFO, "Hello from main!");
+    loguru::add_file("everything.log", loguru::Append, loguru::Verbosity_MAX);
+    loguru::add_file("info.log", loguru::Append, loguru::Verbosity_INFO);
+    loguru::add_file("warning.log", loguru::Append, loguru::Verbosity_WARNING);
+    loguru::add_file("error.log", loguru::Append, loguru::Verbosity_ERROR);
+
+    LOG_F(INFO, "Hello this is an info!");
+    LOG_F(WARNING, "Hello this is a warning!");
+    LOG_F(ERROR, "Hello this is an error!");
+
+    // int a = 2;
+    // int b = 3;
+    // CHECK_EQ_F(a, b, "You can also supply a custom message, like to print something: %d", a + b);
 
     return 0;
 }
