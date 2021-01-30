9de81f345e94be6f572ec1f7c78aafd046416392
Jan S <schaffranek.jan@web.de>
2021-01-30 10:51:58 +0100

    up

--- 3_CMake/10_UnitTesting/include/my_lib.h
+++ 3_CMake/10_UnitTesting/include/my_lib.h
@@ -1,36 +1,9 @@
 #pragma once
 
-#include <cstdint>
+#include "linalg.h"
 
-/**
- * @brief This function prints hello world.
- * 
- */
-void print_hello_world();
-
-/**
- * @brief This function couts hello world.
- * 
- */
 void cout_hello_world();
 
-/**
- * @brief This function prints the content of a vector.
- * 
- */
-void print_linalg_vector();
+linalg::aliases::float3 print_linalg_vector();
 
-/**
- * @brief This function prints the installed boost version.
- * 
- */
-void print_boost_version();
-
-/**
- * @brief This function computes the integer average of two integers.
- * 
- * @param a The first integer value.
- * @param b The second integer value.
- * @return std::int32_t 
- */
-std::int32_t compute_average(std::int32_t a, std::int32_t b);
+bool print_boost_version();
