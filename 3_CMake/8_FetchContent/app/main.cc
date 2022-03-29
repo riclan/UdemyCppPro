64ff666e47f3940736ea26a55a9924ddba728495
franneck94 <schaffranek.jan@web.de>
2022-03-29 14:01:20 +0200

    up

--- 3_CMake/8_FetchContent/app/main.cc
+++ 3_CMake/8_FetchContent/app/main.cc
@@ -1,18 +1,21 @@
 #include <iostream>
 
 #include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
+#include <fmt/format.h>
 
 #include "config.hpp"
 #include "my_lib.h"
 
 int main()
 {
-    std::cout << project_name << ": " << project_version << std::endl;
-
-    std::cout << "Json Lib Version: "
-        << NLOHMANN_JSON_VERSION_MAJOR << "."
-        << NLOHMANN_JSON_VERSION_MINOR << "."
-        << NLOHMANN_JSON_VERSION_PATCH << std::endl;
+    spdlog::info(fmt::format("Welcome to {} v{}\n",
+        project_name,
+        project_version));
+    spdlog::info(fmt::format("Using JSON Lib v{}.{}.{}\n",
+        NLOHMANN_JSON_VERSION_MAJOR,
+        NLOHMANN_JSON_VERSION_MINOR,
+        NLOHMANN_JSON_VERSION_PATCH));
 
     print_hello_world();
 
