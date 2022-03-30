87f846962a0b832a5856b15f3c4674f543fe1fc8
franneck94 <schaffranek.jan@web.de>
2022-03-30 13:04:11 +0200

    up

--- 3_CMake/11_UnitTests/src/my_lib/my_lib.cc
+++ 3_CMake/11_UnitTests/src/my_lib/my_lib.cc
@@ -4,3 +4,8 @@ void print_hello_world()
 {
     std::cout << "Hello World" << std::endl;
 }
+
+unsigned int factorial(unsigned int number)
+{
+    return number <= 1 ? 1 : factorial(number - 1) * number;
+}
--- 3_CMake/11_UnitTests/src/my_lib/my_lib.h
+++ 3_CMake/11_UnitTests/src/my_lib/my_lib.h
@@ -5,3 +5,12 @@
  *
  */
 void print_hello_world();
+
+
+/**
+ * @brief Compute n faculty (n!)
+ *
+ * @param number Input number n
+ * @return Faculty of n
+ */
+unsigned int factorial(unsigned int number);
