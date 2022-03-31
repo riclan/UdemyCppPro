83f2487d596b5668b24a49df06cd02cb78e5a3b3
franneck94 <schaffranek.jan@web.de>
2022-03-31 08:27:51 +0200

    up

--- 3_CMake/12_Final/src/my_lib/my_lib.cc
+++ 3_CMake/12_Final/src/my_lib/my_lib.cc
@@ -2,11 +2,6 @@
 
 #include "my_lib.h"
 
-/**
- * @brief Couts hello world to the console.
- *
- * @return void
- */
 void cout_hello_world()
 {
     std::cout << "Cout: Hello World" << '\n';
--- 3_CMake/12_Final/src/my_lib/my_lib.h
+++ 3_CMake/12_Final/src/my_lib/my_lib.h
@@ -1,5 +1,10 @@
 #pragma once
 
+/**
+ * @brief Couts hello world to the console.
+ *
+ * @return void
+ */
 void cout_hello_world();
 
 unsigned int factorial(unsigned int number);
